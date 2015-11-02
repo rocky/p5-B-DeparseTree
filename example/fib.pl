f102b4e4cba9be63edd4a907deff83126e10d615
Rocky Bernstein <rocky@cpan.org>
2015-11-02 07:23:19 -0500

    Fix some of the bugs in indirop and compile

--- example/fib.pl
+++ example/fib.pl
@@ -12,7 +12,9 @@ sub fib($) {
     fib($x-1) + fib($x-2);
 }
 
-printf "fib(2)= %d, fib(3) = %d, fib(4) = %d\n", fib(2), fib(3), fib(4);
+sub bar {
+    printf "fib(2)= %d, fib(3) = %d, fib(4) = %d\n", fib(2), fib(3), fib(4);
+}
 
 # my $walker = B::Concise::compile('-basic', '-src', 'fib', \&fib);
 # B::Concise::set_style_standard('debug');
@@ -22,12 +24,27 @@ printf "fib(2)= %d, fib(3) = %d, fib(4) = %d\n", fib(2), fib(3), fib(4);
 
 my $deparse = B::DeparseTree->new("-p", "-l", "-c", "-sC");
 
-my @exprs = $deparse->coderef2list(\&fib);
-import Data::Printer colored => 0;
-Data::Printer::p(@exprs);
+# my @exprs = $deparse->coderef2list(\&fib);
+my @exprs = $deparse->coderef2list(\&bar);
+
+# import Data::Printer colored => 0;
+# Data::Printer::p(@exprs);
+
+while (my($key, $value) = each $deparse->{optree}) {
+    printf "0x%x %s | %s", $key, $value->{op}->name, $value->{text};
+    printf " [%s]\n", $value->{cop} ? $value->{cop}->line : 'undef';
+    print '-' x 30, "\n";
+}
+
 print "\n", '-' x 30, "\n";
 print $deparse->coderef2text(\&fib);
 print "\n", '=' x 50, "\n";
 
-my $deparse_old = B::Deparse->new("-p", "-l", "-sC");
-print $deparse_old->coderef2text(\&fib);
+my $walker = B::Concise::compile('-basic', '-src', 'fib', \&fib);
+B::Concise::set_style_standard('debug');
+B::Concise::walk_output(\my $buf);
+$walker->();			# walks and renders into $buf;
+print($buf);
+
+# my $deparse_old = B::Deparse->new("-p", "-l", "-sC");
+# print $deparse_old->coderef2text(\&fib);
